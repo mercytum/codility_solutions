395998309d54937c6703fd25b36e6f56dc242c62
mercytum <mercytum@gmail.com>
2020-08-23 02:34:06 +0300

    Add project files.

---
+++ FrogJmp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FrogJmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(10,85,30));
+        }
+
+        public static int solution(int X, int Y, int D)
+        {
+            if(X >= Y)
+            {
+                return 0;
+            }
+
+            int distanceToCover = Y - X;
+            int jumpsMade = distanceToCover / D;
+            if(distanceToCover % D != 0)
+            {
+                jumpsMade++;
+            }
+            return jumpsMade;
+        }
+    }
+}
