3a87faf23755767c0aacb5914bf6ac9448ebce7b
mercytum <mercytum@gmail.com>
2020-08-23 01:41:49 +0300

    Add project files.

---
+++ Distinct/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Distinct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int solution(int[] A)
+        {
+            HashSet<int> distinctNums = new HashSet<int>();
+
+            foreach(int value in A)
+            {
+                distinctNums.Add(value);
+            }
+
+            return distinctNums.Count;
+        }
+    }
+}
