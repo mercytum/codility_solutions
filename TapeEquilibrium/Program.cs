f67b77eb06b5a73db9e9e8e738b92a0fb021fe07
mercytum <mercytum@gmail.com>
2020-08-23 02:49:00 +0300

    Add project files.

---
+++ TapeEquilibrium/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TapeEquilibrium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = { 3, 1, 2, 4, 3 };
+            Console.WriteLine(solution(test));
+        }
+
+        public static int solution(int[] A)
+        {
+            int smallestDiff = int.MaxValue;
+            int totalSum = 0;
+            
+            foreach(int value in A)
+            {
+                totalSum += value;
+            }
+
+            int firstPart = 0;
+            int secondPart = 0;
+
+            int diff = 0;
+
+            for(int P = 0; P < A.Length - 1; P++)
+            {
+                firstPart += A[P];
+                secondPart = totalSum - firstPart;
+                diff = Math.Abs(firstPart - secondPart);
+                smallestDiff = Math.Min(smallestDiff, diff);
+            }
+
+            return smallestDiff;
+        }
+
+    }
+}
