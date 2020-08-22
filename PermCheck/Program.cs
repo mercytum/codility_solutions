f8240b91e6a0490d6e76e3de2dafee0670bd715a
mercytum <mercytum@gmail.com>
2020-08-23 02:58:49 +0300

    Add project files.

---
+++ PermCheck/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PermCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = { 4, 1, 3};
+            Console.WriteLine(Solution(test));
+        }
+
+        public static int Solution(int[] A)
+        {
+            //find the max value in array
+            int max = A.Max();
+
+            //Put all elements in hashset
+            HashSet<int> numHashset = new HashSet<int>();
+
+            foreach(int value in A)
+            {
+                if(!numHashset.Contains(value))
+                {
+                    numHashset.Add(value);
+                }
+                else
+                {
+                    return 0;
+                }
+                
+            }
+
+            for(int i = 1; i <= max; i++)
+            {
+                if(!numHashset.Contains(i))
+                {
+                    return 0;
+                }
+            }
+            return 1;
+        }
+    }
+}
